acb6c695dbcbe9fd55b1dc3ad015740fcc228321
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-08-12 00:42:17 -0500

    Add files via upload

---
+++ clases.js
@@ -0,0 +1,28 @@
+class Personas{
+    constructor(nombre, apellido, edad){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = edad
+    this.telefono = []
+    };
+    
+    
+    /*conseguirTelefono(){
+        return this.telefono;
+    };*/
+
+    obtenerTelefono(telefono){
+        this.telefono.push(telefono);
+    };
+
+    agregarTelefonos(telefonos) {
+        telefonos.forEach(telefono => this.obtenerTelefono(telefono));
+    }
+}
+
+
+let persona = new Personas("juan", "perez", 18);
+
+persona.agregarTelefonos(["123-456-7890", "987-654-3210"]);
+
+console.log(persona)
